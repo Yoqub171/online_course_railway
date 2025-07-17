712abb2eea3cf880cd071abf10073ce05ac5dd2c
Yoqub <mardonovyoqubjon50@gmail.com>
2025-07-16 22:58:33 -0700

    ✅ fix static path

--- config/settings.py
+++ config/settings.py
@@ -133,9 +133,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    BASE_DIR / 'shop' / 'static',
-    BASE_DIR / 'users' / 'static',
+    BASE_DIR / "shop" / "static",
+    BASE_DIR / "users" / "static",
 ]
+
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
