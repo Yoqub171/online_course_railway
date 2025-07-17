e81e25bcbe728674398ff4f74c46be157149d015
Yoqub <mardonovyoqubjon50@gmail.com>
2025-07-16 20:10:16 -0700

    Fix: hardcoded ALLOWED_HOSTS

--- config/settings.py
+++ config/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG") == "True"
 
-ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "").split(",")
+ALLOWED_HOSTS = ['web-production-f04f.up.railway.app']
 
 
 
